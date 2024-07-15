d2a1fe75eb57378a1f251ef4b08314ec44a40a18
Utkarsh Jain <xutkarshjain.gmail.com>
2024-07-16 01:35:33 +0530

    Created sections with mock data

--- frontend/googleForm/src/app/create-form/create-form.component.ts
+++ frontend/googleForm/src/app/create-form/create-form.component.ts
@@ -6,7 +6,97 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-form.component.css'],
 })
 export class CreateFormComponent implements OnInit {
+  sectionName: string = 'Untitled Section';
+  sectionDescription: string = '';
+  selectedId: any;
+
+  sections: any = [
+    {
+      sectionName: 'Untitled Section',
+      sectionId: 1,
+      sectionDescription: '',
+      selected: true,
+      shuffleQuestions: true,
+      sectionPriority: 1,
+      questions: [
+        {
+          questionId: 1,
+          questionText: 'What is your age?',
+          questionType: 'MCQ',
+          required: true,
+          shuffleOptions: true,
+          questionPriority: 1,
+          options: [
+            {
+              optionId: 1,
+              optionText: 'Ramesh',
+              optionPriority: 1,
+            },
+          ],
+        },
+        {
+          questionId: 2,
+          questionText: 'What is your name?',
+          questionType: 'MCQ',
+          required: true,
+          shuffleOptions: true,
+          questionPriority: 1,
+          options: [
+            {
+              optionId: 1,
+              optionText: 'Ramesh',
+              optionPriority: 1,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      sectionName: 'Untitled Section',
+      sectionId: 2,
+      sectionDescription: '',
+      selected: true,
+      shuffleQuestions: true,
+      sectionPriority: 1,
+      questions: [
+        {
+          questionId: 1,
+          questionText: 'What is your age?',
+          questionType: 'MCQ',
+          required: true,
+          shuffleOptions: true,
+          questionPriority: 1,
+          options: [
+            {
+              optionId: 1,
+              optionText: 'Ramesh',
+              optionPriority: 1,
+            },
+          ],
+        },
+        {
+          questionId: 2,
+          questionText: 'What is your name?',
+          questionType: 'MCQ',
+          required: true,
+          shuffleOptions: true,
+          questionPriority: 1,
+          options: [
+            {
+              optionId: 1,
+              optionText: 'Ramesh',
+              optionPriority: 1,
+            },
+          ],
+        },
+      ],
+    },
+  ];
   ngOnInit(): void {
     console.log('oninit');
+  }
+
+  updateSelected(id: any) {
+    this.selectedId = id;
   }
 }

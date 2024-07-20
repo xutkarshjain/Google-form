c4295d828014ebabae35b6289a70d550ff573683
Utkarsh Jain <xutkarshjain.gmail.com>
2024-07-21 02:58:12 +0530

    Sample and default data added in form

--- frontend/googleForm/src/app/create-form/create-form.component.ts
+++ frontend/googleForm/src/app/create-form/create-form.component.ts
@@ -1,11 +1,6 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  Validator,
-  FormArray,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-create-form',
@@ -19,72 +14,169 @@ export class CreateFormComponent implements OnInit {
     type: '',
   };
 
-  sectionsArray: any = [
-    {
-      sectionName: 'Untitled Section',
-      sectionId: 1,
-      sectionDescription: '',
-      shuffleQuestions: true,
-      sectionPriority: 1,
-      questions: [
-        {
-          questionId: 3,
-          questionText: 'What is your age?',
-          questionType: 'MCQ',
-          required: true,
-          shuffleOptions: true,
-          questionPriority: 1,
-          options: [
-            {
-              optionId: 1,
-              optionText: 'Ramesh',
-              optionPriority: 1,
-            },
-            {
-              optionId: 2,
-              optionText: 'Suresh',
-              optionPriority: 2,
-            },
-            {
-              optionId: 1,
-              optionText: 'Mahesh',
-              optionPriority: 3,
-            },
-          ],
-        },
-        {
-          questionId: 4,
-          questionText: 'What is your name?',
-          questionType: 'MCQ',
-          required: true,
-          shuffleOptions: true,
-          questionPriority: 1,
-          options: [
-            {
-              optionId: 1,
-              optionText: 'Ramesh',
-              optionPriority: 1,
-            },
-          ],
-        },
-      ],
-    },
-  ];
+  defaultData: any = {
+    formName: 'Untitled Form',
+    formId: null,
+    sections: [
+      {
+        id: null,
+        name: 'Untitled Section',
+        description: '',
+        priority: 1,
+        shuffle: false,
+        questions: [
+          {
+            id: null,
+            text: 'Question',
+            type: 'Multiple choice',
+            shuffle: false,
+            required: false,
+            priority: 1,
+            options: [
+              {
+                id: null,
+                text: 'option',
+                priority: 1,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
 
-  parentForm: FormGroup;
+  responseData: any = {
+    formName: 'My form',
+    formId: null,
+    sections: [
+      {
+        id: 1,
+        name: 'section-1',
+        description: 'desc-1',
+        priority: 1,
+        shuffle: true,
+        questions: [
+          {
+            id: 1,
+            text: 'question-1',
+            type: 'Checkboxes',
+            shuffle: true,
+            required: true,
+            priority: 1,
+            options: [
+              {
+                id: 1,
+                text: 'option-1',
+                priority: 1,
+              },
+              {
+                id: 2,
+                text: 'option-2',
+                priority: 2,
+              },
+              {
+                id: 3,
+                text: 'option-3',
+                priority: 3,
+              },
+            ],
+          },
+          {
+            id: 2,
+            text: 'question-2',
+            type: 'Multiple choice',
+            shuffle: false,
+            required: false,
+            priority: 2,
+            options: [
+              {
+                id: 1,
+                text: 'option-2.1',
+                priority: 1,
+              },
+              {
+                id: 2,
+                text: 'option-2.2',
+                priority: 2,
+              },
+              {
+                id: 3,
+                text: 'option-2.3',
+                priority: 3,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: 'section-2',
+        description: 'desc-2',
+        priority: 2,
+        shuffle: true,
+        questions: [
+          {
+            id: 3,
+            text: 'question-2.1',
+            type: 'Multiple choice',
+            shuffle: true,
+            required: true,
+            priority: 1,
+            options: [
+              {
+                id: 1,
+                text: 'option-1',
+                priority: 1,
+              },
+              {
+                id: 2,
+                text: 'option-2',
+                priority: 2,
+              },
+              {
+                id: 3,
+                text: 'option-3',
+                priority: 3,
+              },
+            ],
+          },
+          {
+            id: 2,
+            text: 'question-2',
+            type: 'Multiple choice',
+            shuffle: false,
+            required: false,
+            priority: 2,
+            options: [
+              {
+                id: 1,
+                text: 'option-2.1',
+                priority: 1,
+              },
+              {
+                id: 2,
+                text: 'option-2.2',
+                priority: 2,
+              },
+              {
+                id: 3,
+                text: 'option-2.3',
+                priority: 3,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  parentForm!: FormGroup;
   questionTypes: any = [
     { value: 'Multiple choice', viewValue: 'Multiple choice' },
     { value: 'Checkboxes', viewValue: 'Checkboxes' },
   ];
 
-  constructor(private fb: FormBuilder) {
-    this.parentForm = this.fb.group({
-      formName: ['Untitled form', Validators.required],
-      formId: '',
-      sections: fb.array([]),
-    });
-    this.addSection();
-  }
+  constructor(private fb: FormBuilder) {}
 
   get sections(): FormArray {
     return this.parentForm.get('sections') as FormArray;
@@ -100,41 +192,48 @@ export class CreateFormComponent implements OnInit {
       .get('options') as FormArray;
   }
 
-  addSection() {
+  addSection(data?: object) {
+    let formData = data || this.defaultData.sections[0];
     const section = this.fb.group({
-      id: null,
-      name: ['Untitled section', Validators.required],
-      description: ['Description'],
-      priority: null,
-      shuffle: false,
+      id: formData.id || null,
+      name: [formData.name, Validators.required],
+      description: formData.description,
+      priority: formData.priority,
+      shuffle: formData.shuffle,
       questions: this.fb.array([]),
     });
     this.sections.push(section);
-    this.addQuestion(this.sections.length - 1);
+    for (let question of formData.questions) {
+      this.addQuestion(this.sections.length - 1, question);
+    }
   }
 
-  addQuestion(sectionIndex: number) {
+  addQuestion(sectionIndex: number, data?: object) {
+    let formData = data || this.defaultData.sections[0].questions[0];
     const question = this.fb.group({
-      id: null,
-      text: ['Question', Validators.required],
-      type: 'Multiple choice',
-      shuffle: false,
-      required: false,
-      priority: null,
+      id: formData.id,
+      text: [formData.text, Validators.required],
+      type: formData.type,
+      shuffle: formData.shuffle,
+      required: formData.required,
+      priority: formData.priority,
       options: this.fb.array([]),
     });
 
     this.getQuestions(sectionIndex).push(question);
     let questionIndex: number = this.getQuestions(sectionIndex).length - 1;
-    this.addOption(sectionIndex, questionIndex);
+    for (let option of formData.options) {
+      this.addOption(sectionIndex, questionIndex, option);
+    }
     this.updateSelected(sectionIndex, questionIndex, 'question');
   }
 
-  addOption(sectionIndex: number, questionIndex: number) {
+  addOption(sectionIndex: number, questionIndex: number, data?: object) {
+    let formData = data || this.defaultData.sections[0].questions[0].options[0];
     const option = this.fb.group({
-      id: '',
-      text: ['option', Validators.required],
-      priority: '',
+      id: formData.id,
+      text: [formData.id, Validators.required],
+      priority: formData.priority,
     });
     this.getOptions(sectionIndex, questionIndex).push(option);
   }
@@ -157,6 +256,18 @@ export class CreateFormComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('parentForm', this.parentForm);
+    //check whether mode is edit or create
+    // if create, initialise with empty data, else call fetch by Id API
+    let FormData = this.responseData;
+
+    this.parentForm = this.fb.group({
+      formName: [FormData.formName, Validators.required],
+      formId: FormData.formId,
+      sections: this.fb.array([]),
+    });
+    for (let section of FormData.sections) {
+      this.addSection(section);
+    }
   }
 
   updateSelected(sectionIndex: number, questionIndex: number, type: string) {
